0052c111a935429a1521c7648a37e6954d002f48
yamako6321 <124650861+yamako6321@users.noreply.github.com>
2023-03-09 21:09:35 +0700

    def okrgnst, def triangle

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
 import turtle
 import math
 #Group: Igor Kuznetcov, Maria Yadreeva
-def okrgnst(x,y,rds,color,p_color):
+def okrgnst(
+        x, y, rds,
+        color, p_color):
     h.speed(40)
     h.pensize(4)
     h.color(color, p_color)
     h.penup()
-    h.goto(x,y)
+    h.goto(x, y)
     h.begin_fill()
     h.pendown()
     h.circle(rds)
     h.end_fill()
-h=turtle.Turtle()
-
-def triangle(x,y,gipotenuza,h,color,p_color):
-    sin=h/gipotenuza
-    ugol1=math.asin(sin)*180/math.pi
-    ugol=180-ugol1
+h = turtle.Turtle()
+okrgnst(10,10,60,'red', 'pink')
+def triangle(x, y, gipotenuza,
+             h, color, p_color):
+    sin = h/gipotenuza
+    ugol1 = math.asin(sin) * 180/math.pi
+    ugol = 180-ugol1
     t.speed(10)
     t.pensize(5)
-    t.color(color,p_color)
+    t.color(color, p_color)
     t.penup()
-    t.goto(x,y)
+    t.goto(x, y)
     t.begin_fill()
     t.pendown()
     t.forward(((gipotenuza**2-h**2)**0.5)*2)
     t.left(ugol)
     t.forward(gipotenuza)
-    t.left(2*ugol1)
+    t.left(2 * ugol1)
     t.forward(gipotenuza)
     t.end_fill()
-    t.left(360+180-ugol1)
-t=turtle.Turtle()
+    t.left(360 + 180 - ugol1)
+t = turtle.Turtle()
+triangle(-100,-100,90,70,'blue','brown')
 turtle.exitonclick()
