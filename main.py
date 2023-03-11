cf2736e151ebb755d24289f4895dc6bc69562934
yamako6321 <124650861+yamako6321@users.noreply.github.com>
2023-03-11 12:06:53 +0700

    def circle, def triangle

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 import turtle
 import math
 #Group: Igor Kuznetcov, Maria Yadreeva
-def okrgnst(x, y, rds,
-            color, p_color):
-    h.speed(40)
-    h.pensize(4)
-    h.color(color, p_color)
-    h.penup()
-    h.goto(x, y)
-    h.begin_fill()
-    h.pendown()
-    h.circle(rds)
-    h.end_fill()
-h = turtle.Turtle()
-def triangle(x, y, gipotenuza,
-             h, color, p_color):
-    sin = h/gipotenuza
-    ugol1 = math.asin(sin) * 180/math.pi
-    ugol = 180-ugol1
-    t.speed(10)
-    t.pensize(5)
-    t.color(color, p_color)
+def circle(x, y, rds,
+            clr, p_clr):
+    t.speed(40)
+    t.pensize(4)
+    t.color(clr, p_clr)
     t.penup()
     t.goto(x, y)
     t.begin_fill()
     t.pendown()
-    t.forward(((gipotenuza**2-h**2)**0.5)*2)
+    t.circle(rds)
+    t.end_fill()
+t = turtle.Turtle()
+def triangle(x, y, hypotenuse,
+             h, clr, p_clr):
+    crtn_ngl = h/hypotenuse
+    ugol1 = math.asin(crtn_ngl) * 180/math.pi
+    ugol = 180-ugol1
+    t.speed(10)
+    t.pensize(5)
+    t.color(clr, p_clr)
+    t.penup()
+    t.goto(x, y)
+    t.begin_fill()
+    t.pendown()
+    t.forward(((hypotenuse**2-h**2)**0.5)*2)
     t.left(ugol)
-    t.forward(gipotenuza)
+    t.forward(hypotenuse)
     t.left(2 * ugol1)
-    t.forward(gipotenuza)
+    t.forward(hypotenuse)
     t.end_fill()
     t.left(360 + 180 - ugol1)
 
